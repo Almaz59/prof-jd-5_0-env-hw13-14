7893ae9f820855cb70f3557bd5372c206787fdb3
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-06-12 15:31:19 +0500

    hw1314part2 first commit

---
+++ src/main/java/pro/sky/profjd5_0envhw1314/ProfJd50EnvHw1314Application.java
@@ -0,0 +1,13 @@
+package pro.sky.profjd5_0envhw1314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfJd50EnvHw1314Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfJd50EnvHw1314Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/profjd5_0envhw1314/ProfJd50EnvHw1314ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.profjd5_0envhw1314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfJd50EnvHw1314ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
